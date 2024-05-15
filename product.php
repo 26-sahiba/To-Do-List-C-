68f46d31c809ea7d07235ea5a9cfd3b25a42e2f5
shakiel17 <hisoka.aboy@gmail.com>
2024-05-15 08:30:43 +0800

    Inventory System

---
+++ product.php
@@ -0,0 +1,89 @@
+<?php
+  $page_title = 'All Product';
+  require_once('includes/load.php');
+  // Checkin What level user has permission to view this page
+   page_require_level(2);
+  $products = join_product_table();
+  $search_product=array();
+  if(isset($_POST['submit'])){
+    $products=find_product_table($_POST['description']);  
+  }  
+?>
+<?php include_once('layouts/header.php'); ?>
+  <div class="row">
+     <div class="col-md-12">
+       <?php echo display_msg($msg); ?>
+     </div>
+    <div class="col-md-12">
+      <div class="panel panel-default">
+        <div class="panel-heading clearfix">
+          <div class="pull-left">            
+           <form method="POST" action="product.php">
+            <table width="100%">
+              <tr>
+                <td><input type="text" class="form-control" name="description" placeholder="Search product here"></td>
+                <td><input type="submit" name="submit" value="Search" class="btn btn-success btn-sm">
+            <a href="product.php" class="btn btn-default btn-sm">Refresh</a></td>
+              </tr>
+            </table>                        
+           </form>
+         </div>
+         <div class="pull-right">
+           <a href="add_product.php" class="btn btn-primary">Add New</a>
+         </div>
+        </div>
+        <div class="panel-body">
+          <table class="table table-bordered">
+            <thead>
+              <tr>
+                <th class="text-center" style="width: 50px;">#</th>
+                <th> </th>
+                <th> Product Title </th>
+                <th class="text-center" style="width: 10%;"> Categories </th>
+                <th class="text-center" style="width: 10%;"> In-Stock </th>
+                <th class="text-center" style="width: 10%;"> Buying Price </th>
+                <th class="text-center" style="width: 10%;"> Selling Price </th>
+                <th class="text-center" style="width: 10%;"> Product Added </th>
+                <th class="text-center" style="width: 100px;"> Actions </th>
+              </tr>
+            </thead>
+            <tbody>
+             
+              <?php foreach ($products as $product):?>
+              <tr>
+                <td class="text-center"><?php echo count_id();?></td>
+                <td>
+                  <?php if($product['media_id'] === '0'): ?>
+                    <img class="img-avatar img-circle" src="uploads/products/no_image.png" alt="">
+                  <?php else: ?>
+                  <img class="img-avatar img-circle" src="uploads/products/<?php echo $product['image']; ?>" alt="">
+                <?php endif; ?>
+                </td>
+                <td> <?php echo remove_junk($product['name']); ?></td>
+                <td class="text-center"> <?php echo remove_junk($product['categorie']); ?></td>
+                <td class="text-center"> <?php echo remove_junk($product['quantity']); ?></td>
+                <td class="text-center"> <?php echo remove_junk($product['buy_price']); ?></td>
+                <td class="text-center"> <?php echo remove_junk($product['sale_price']); ?></td>
+                <td class="text-center"> <?php echo read_date($product['date']); ?></td>
+                <td class="text-center">
+                  <div class="btn-group">
+                    <a href="edit_product.php?id=<?php echo (int)$product['id'];?>" class="btn btn-info btn-xs"  title="Edit" data-toggle="tooltip">
+                      <span class="glyphicon glyphicon-edit"></span>
+                    </a> 
+                    <!-- <a href="delete_product.php?id=<?php echo (int)$product['id'];?>" class="btn btn-danger btn-xs"  title="Delete" data-toggle="tooltip">
+                      <span class="glyphicon glyphicon-trash"></span>
+                    </a> -->
+                    <a href="add_product_quantity.php?id=<?php echo (int)$product['id'];?>" class="btn btn-danger btn-xs"  title="Stock in" data-toggle="tooltip">
+                      <span class="glyphicon glyphicon-plus"></span>
+                    </a>
+                  </div>
+                </td>
+              </tr>
+             <?php endforeach; ?>
+            </tbody>
+          </tabel>
+        </div>
+      </div>
+    </div>
+  </div>
+  <?php include_once('layouts/footer.php'); ?>
---
+++ sales_report_stock.php
@@ -0,0 +1,143 @@
+<?php
+$page_title = 'Sales Report';
+$results = '';
+  require_once('includes/load.php');
+  // Checkin What level user has permission to view this page
+   page_require_level(3);
+?>
+<?php
+if(isset($_POST['submit'])){
+    $req_dates = array('start-date','end-date');
+    validate_fields($req_dates);
+
+    if(empty($errors)):
+      $start_date   = remove_junk($db->escape($_POST['start-date']));
+      $end_date     = remove_junk($db->escape($_POST['end-date']));
+      $results      = find_sale_by_dates($start_date,$end_date);
+    else:
+      $session->msg("d", $errors);
+      redirect('sales_report.php', false);
+    endif;
+
+  } else {
+    $session->msg("d", "Select dates");
+    redirect('sales_report.php', false);
+  }  
+  $products = join_product_table();
+?>
+<!doctype html>
+<html lang="en-US">
+ <head>
+   <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+   <title>Default Page Title</title>
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css"/>
+   <style>
+   @media print {
+     html,body{
+        font-size: 9.5pt;
+        margin: 0;
+        padding: 0;
+     }.page-break {
+       page-break-before:always;
+       width: auto;
+       margin: auto;
+      }
+    }
+    .page-break{
+      width: 980px;
+      margin: 0 auto;
+    }
+     .sale-head{
+       margin: 40px 0;
+       text-align: center;
+     }.sale-head h1,.sale-head strong{
+       padding: 10px 20px;
+       display: block;
+     }.sale-head h1{
+       margin: 0;
+       border-bottom: 1px solid #212121;
+     }.table>thead:first-child>tr:first-child>th{
+       border-top: 1px solid #000;
+      }
+      table thead tr th {
+       text-align: center;
+       border: 1px solid #ededed;
+     }table tbody tr td{
+       vertical-align: middle;
+     }.sale-head,table.table thead tr th,table tbody tr td,table tfoot tr td{
+       border: 1px solid #212121;
+       white-space: nowrap;
+     }.sale-head h1,table thead tr th,table tfoot tr td{
+       background-color: #f8f8f8;
+     }tfoot{
+       color:#000;
+       text-transform: uppercase;
+       font-weight: 500;
+     }
+   </style>
+</head>
+<body>
+  <?php if($results): ?>
+    <div class="page-break">
+       <div class="sale-head">
+           <h1>Drifter Concept Vehicle Trading - Stock Inventory</h1>
+           <strong><?php if(isset($start_date)){ echo $start_date;}?> TILL DATE <?php if(isset($end_date)){echo $end_date;}?> </strong>
+       </div>
+      <table class="table table-border">
+        <thead>
+          <tr>              
+              <th>Product Title</th>              
+              <th>Stock In</th>
+              <th>Qty Sold</th>     
+              <th>SOH</th>         
+          </tr>
+        </thead>
+        <tbody>
+          <?php foreach($products as $result): ?>
+            <?php
+            $sold=find_sale_by_item($result['id'],$start_date,$end_date);
+            $stockin=find_stock_by_item($result['id'],$start_date,$end_date);
+            $sold_item=0;
+            foreach($sold as $x){
+              $sold_item += $x['qty_sold'];
+            }
+            $in_item=0;
+            foreach($stockin as $x){
+              $in_item += $x['qty_in'];
+            }
+            ?>
+           <tr>              
+              <td class="desc">
+                <h6><?php echo remove_junk(ucfirst($result['name']));?></h6>
+              </td>
+              <td class="text-center"><?php echo remove_junk($in_item);?></td>
+              <td class="text-center"><?php echo remove_junk($sold_item);?></td>              
+              <td class="text-center"><?php echo remove_junk($result['quantity']);?></td>
+          </tr>
+        <?php endforeach; ?>
+        </tbody>
+        <!-- <tfoot>
+         <tr class="text-right">
+           <td colspan="4"></td>
+           <td colspan="1">Grand Total</td>
+           <td> ₱
+           <?php echo number_format(total_price($results)[0], 2);?>
+          </td>
+         </tr>
+         <tr class="text-right">
+           <td colspan="4"></td>
+           <td colspan="1">Profit</td>
+           <td> ₱<?php echo number_format(total_price($results)[1], 2);?></td>
+         </tr>
+        </tfoot> -->
+      </table>
+    </div>
+  <?php
+    else:
+        $session->msg("d", "Sorry no sales has been found. ");
+        redirect('sales_report.php', false);
+     endif;
+  ?>
+</body>
+</html>
+<?php if(isset($db)) { $db->db_disconnect(); } ?>
